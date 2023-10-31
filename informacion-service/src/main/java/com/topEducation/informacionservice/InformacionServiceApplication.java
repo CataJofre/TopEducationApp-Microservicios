c89ab7e4dfcda6b9fc3511dedce1bcc530f3d57f
CataJofre <catalina.jofre.l@usach.cl>
2023-10-31 19:05:01 -0300

    Adición de configuraciones

--- informacion-service/src/main/java/com/topEducation/informacionservice/InformacionServiceApplication.java
+++ informacion-service/src/main/java/com/topEducation/informacionservice/InformacionServiceApplication.java
@@ -1,13 +1,13 @@
-package com.topEducation.infoEstudiantesservice;
+package com.topEducation.informacionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InfoEstudiantesServiceApplication {
+public class InformacionServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(InfoEstudiantesServiceApplication.class, args);
+		SpringApplication.run(InformacionServiceApplication.class, args);
 	}
 
 }
