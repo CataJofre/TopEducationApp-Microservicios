2b94777fcdffcc6066e31d99918d7934c63f0b6d
CataJofre <catalina.jofre.l@usach.cl>
2023-11-09 14:27:54 -0300

    cambio

--- estudiante-service/src/main/java/com/topEducation/estudianteservice/Config/RestTemplateConfig.java
+++ estudiante-service/src/main/java/com/topEducation/estudianteservice/Config/RestTemplateConfig.java
@@ -4,9 +4,6 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 public class RestTemplateConfig {
